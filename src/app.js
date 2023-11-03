9e65e5b28162ec5926a61664f627bddd6bde4fb0
Jo Young <jo.young@digital.cabinet-office.gov.uk>
2023-11-03 10:54:44 +0000

    Add node express app and basic layout

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const path = require("path");
+const { configureNunjucks } = require("./config/nunjucks");
+const app = express();
+const port = 3000;
+
+const APP_VIEWS = [
+  path.join(__dirname, "views"),
+  path.resolve("node_modules/govuk-frontend/"),
+  path.resolve("node_modules/@govuk-prototype-kit/templates"),
+];
+
+app.set("view engine", configureNunjucks(app, APP_VIEWS));
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "/node_modules/govuk-frontend/govuk/assets"))
+);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.njk");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
