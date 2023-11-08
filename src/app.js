bdcabc4bd7c6d71c64c84d82d416ad3782ffd039
di-aholme <andy.holme@digital.cabinet-office.gov.uk>
2023-11-08 15:22:04 +0000

    [DFC-75] Adds ga4 package to repo and initialises the tag in base.njk

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const { configureNunjucks } = require("./config/nunjucks");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const APP_VIEWS = [
   path.join(__dirname, "views"),
@@ -11,16 +11,24 @@ const APP_VIEWS = [
 ];
 
 app.set("view engine", configureNunjucks(app, APP_VIEWS));
+
 app.use(
   "/assets",
   express.static(
-    path.join(__dirname, "/node_modules/govuk-frontend/govuk/assets"),
+    path.join(__dirname, "../node_modules/govuk-frontend/govuk/assets"),
   ),
 );
+
+/**GA4 assets */
+app.use(
+  "/ga4-assets",
+  express.static(path.join(__dirname, "../node_modules/one-login-ga4/lib")),
+);
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.njk");
+  res.render("index.njk", { ga4ContainerId: "GTM-KD86CMZ" });
 });
 
 app.get("/service-description", (req, res) => {
