ad493158e1b143d17fd6c856043cd032f2360e33
Sophia Wokoma <sophia.wokoma@digital.cabinet-office.gov.uk>
2023-11-17 11:34:36 +0000

    added condition to check if formData is null or undefined

--- src/validateForm.js
+++ src/validateForm.js
@@ -1,5 +1,5 @@
 const validateForm = (formData, nextPage) => {
-  if (formData.length == 0) {
+  if (!formData || !formData.length) {
     console.log(`Error: No option selected. Setting showError to true.`);
     return { showError: true };
   } else {
