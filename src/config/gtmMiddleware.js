98f88900d81fae198a7d5637dac1cd7d7bb5b960
Sophia Wokoma <sophia.wokoma@digital.cabinet-office.gov.uk>
2023-12-01 11:25:13 +0000

    add console.logs to debug

--- src/config/gtmMiddleware.js
+++ src/config/gtmMiddleware.js
@@ -18,7 +18,9 @@ const setStatusCode = (req, res, next) => {
 // Middleware to instantiate the values for taxonomy levels 1 and 2 for the On Page Load tracker
 const setTaxonomyValues = (req, res, next) => {
   const url = req.url;
-  const pathFound = ROUTE_INFO.find((route) => route.path === url.split("?")[0]);
+  const pathFound = ROUTE_INFO.find(
+    (route) => route.path === url.split("?")[0]
+  );
   if (pathFound) {
     res.locals.taxonomyLevel1 = pathFound.taxonomyLevel1 || "undefined";
     res.locals.taxonomyLevel2 = pathFound.taxonomyLevel2 || "undefined";
@@ -34,7 +36,9 @@ const setTaxonomyValues = (req, res, next) => {
 // Middleware to instantiate the value for the pageTitle for the On Page Load tracker
 const setPageTitle = (req, res, next) => {
   const url = req.url;
-  const pathFound = ROUTE_INFO.find((route) => route.path === url.split("?")[0]);
+  const pathFound = ROUTE_INFO.find(
+    (route) => route.path === url.split("?")[0]
+  );
 
   if (pathFound) {
     res.locals.englishPageTitle = pathFound.pageTitle || "undefined";
@@ -49,9 +53,12 @@ const setPageTitle = (req, res, next) => {
 // Middleware to instantiate the value for the pageTitle for the On Page Load tracker
 const setContentId = (req, res, next) => {
   const url = req.url;
-  const pathFound = ROUTE_INFO.find((route) => route.path === url.split("?")[0]);
+  const pathFound = ROUTE_INFO.find(
+    (route) => route.path === url.split("?")[0]
+  );
 
   if (pathFound) {
+    console.log(pathFound.path);
     res.locals.contentId = pathFound.contentId || "undefined";
   } else {
     console.log("Path not found");
--- src/config/middleware.js
+++ src/config/middleware.js
@@ -10,6 +10,9 @@ const checkSessionAndRedirect = (req, res, next) => {
 
   // If the user is on the Home Page and does not have a session, set it
   if (isOnHomepage && !hasSession) {
+    console.log(
+      `user has no session but is on the HP , so lets set the session id`
+    );
     req.session.userSession = {
       startedJourney: true,
     };
@@ -17,10 +20,30 @@ const checkSessionAndRedirect = (req, res, next) => {
 
   // If the user doesn't have a session and is not on the homepage, redirect to Journey Guard Page but if entering on / take them to welcome page
   if (!hasSession && !isOnHomepage) {
-    return res.render("journeyGuard.njk");
+    if (req.path === "/") {
+      console.log(
+        `user is not on HP and does not have a session, the path is ${req.path} so lets redirect to welcome`
+      );
+      return res.redirect("./welcome");
+    } else {
+      console.log(
+        `user is not on HP and does not have a session, the path is ${req.path}`
+      );
+      return res.render("journeyGuard.njk");
+    }
   }
   // If the user has a session and the path is / redirect to Home Page path which is "/welcome"
   if (hasSession && req.path === "/") {
+    console.log(
+      `user has session but the path is ${req.path}, let redirect to /welcome`
+    );
+    return res.redirect("/welcome");
+  }
+
+  if (!hasSession && req.path === "/") {
+    console.log(
+      `user has no session but the path is ${req.path}, let redirect to /welcome`
+    );
     return res.redirect("/welcome");
   }
 
