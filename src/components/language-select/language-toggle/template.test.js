07e1654cbc11aec501c67c5f02cbf8173cffd597
joanneyoung01 <jo.young@digital.cabinet-office.gov.uk>
2024-02-06 15:29:04 +0000

    Add changes to accessibility and unit tests

---
+++ src/components/language-select/language-toggle/template.test.js
@@ -0,0 +1,97 @@
+const nunjucks = require("nunjucks");
+const fs = require("fs");
+const path = require("path");
+
+const { axe, toHaveNoViolations } = require("jest-axe");
+const { render } = require("../../../utils/jestHelpers");
+
+expect.extend(toHaveNoViolations);
+
+const templatePath = "src/components/language-select/language-toggle";
+nunjucks.configure(path.dirname(templatePath), { autoescape: true });
+
+describe("oneloginLanguageSelect Component", () => {
+  const mockEnParams = {
+    ariaLabel: "test-aria",
+    language: "en",
+    class: "test-class",
+    languages: [
+      {
+        code: "en",
+        text: "English"
+      },
+      {
+        code: "cy",
+        text: "Cymraeg"
+      }
+    ]
+  };
+
+  it("has the appropriate accessibility testing", async () => {
+    const renderedComponent = render(
+      "language-toggle",
+      "oneloginLanguageSelect",
+      mockEnParams
+    );
+
+    const results = await axe(renderedComponent.html());
+    expect(results).toHaveNoViolations();
+  });
+
+  it("renders the aria-label from params", () => {
+    const renderedComponent = render(
+      "language-toggle",
+      "oneloginLanguageSelect",
+      mockEnParams
+    );
+    const renderedNavElement = renderedComponent("nav");
+    expect(renderedNavElement.attr("class")).toContain("test-class");
+  });
+
+  it("renders the class from params", () => {
+    const renderedComponent = render(
+      "language-toggle",
+      "oneloginLanguageSelect",
+      mockEnParams
+    );
+    const renderedNavElement = renderedComponent("nav");
+    expect(renderedNavElement.attr("aria-label")).toBe("test-aria");
+  });
+
+  it("renders the active language as a span, and inactive language as a link", () => {
+    const mockCyParams = {
+      ariaLabel: "test-aria",
+      language: "cy",
+      class: "test-class",
+      languages: [
+        {
+          code: "en",
+          text: "English"
+        },
+        {
+          code: "cy",
+          text: "Cymraeg"
+        }
+      ]
+    };
+
+    const renderedComponent = render(
+      "language-toggle",
+      "oneloginLanguageSelect",
+      mockCyParams
+    );
+
+    // test span
+    const renderedSpan = renderedComponent("span").text();
+    expect(renderedSpan).toBe("Cymraeg");
+
+    // test link
+    const renderedLink = renderedComponent(".govuk-link");
+    expect(renderedLink.get(0).tagName).toEqual("a");
+    expect(renderedLink.attr("target")).toEqual(undefined);
+    expect(renderedLink.attr("href")).toContain("?lng=en");
+    expect(renderedLink.attr("class")).toContain(
+      "govuk-link govuk-link--no-visited-state"
+    );
+  });
+});
