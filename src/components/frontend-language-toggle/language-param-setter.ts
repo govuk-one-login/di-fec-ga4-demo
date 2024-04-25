b08db0b479fcabf3c9bbb81ed54ea6bc4d8df1e0
di-aholme <andy.holme@digital.cabinet-office.gov.uk>
2024-04-25 11:39:37 +0100

    Language Toggle/DFC-551: Modify param component to use ES6 syntax

--- src/components/frontend-language-toggle/language-param-setter.ts
+++ src/components/frontend-language-toggle/language-param-setter.ts
@@ -1,4 +1,4 @@
-module.exports = (language, url) => {
+export default function addLanguageParam(language: string, url: string) {
   const parsedUrl = new URL(url);
   parsedUrl.searchParams.set("lng", language);
   return parsedUrl.pathname + parsedUrl.search;
